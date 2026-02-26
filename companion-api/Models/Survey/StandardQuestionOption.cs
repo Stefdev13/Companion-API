442bb7ad939d8cb357cf56d198ed53da1f44f357
Stefdev13 <stef.bekaert@proton.me>
2026-02-26 11:04:17 +0100

    StandardQuestionOption
    Set some default values for StandardQuestionOption

--- companion-api/Models/Survey/StandardQuestionOption.cs
+++ companion-api/Models/Survey/StandardQuestionOption.cs
@@ -2,7 +2,7 @@ public class StandardQuestionOption : QuestionOption
 {
     public required List<SubQuestion> SubQuestions { get; set; } = [];
     public ReusableQuestionOption? ReusableQuestionOption { get; set; }
-    public bool UsedInOtherSurveyQuestions { get; set; }
-    public bool CloneOfOtherQuestionOption { get; set; }
-    public bool ShouldInfluenceOriginalQuestionOption { get; set; }
+    public bool UsedInOtherSurveyQuestions { get; set; } = false;
+    public bool CloneOfOtherQuestionOption { get; set; } = false;
+    public bool ShouldInfluenceOriginalQuestionOption { get; set; } = false;
 }
